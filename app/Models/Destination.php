d9a2bb8b0a1a56b6b526d52112e9819b53a35214
marginalfriend <abdullahpsg@gmail.com>
2024-06-28 23:15:02 +0700

    [NEW] relationship definition

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Destination extends Model
 {
     use HasFactory;
+
+		public function category(): BelongsTo
+		{
+			return $this->belongsTo(Category::class);
+		}
+
+		public function orders(): HasMany
+		{
+			return $this->hasMany(Order::class);
+		}
 }
