1e62b8b9ffe8e0a2630d500e8102456cdd90df3e
marginalfriend <abdullahpsg@gmail.com>
2024-06-29 00:05:56 +0700

    [NEW] destinations migration

--- database/migrations/2024_06_28_155624_create_categories_table.php
+++ database/migrations/2024_06_28_155624_create_categories_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('category', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+						$table->string('name');
         });
     }
 
