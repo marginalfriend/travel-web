8a74c6f62ecb51588faf747a68c2515fc113109a
marginalfriend <abdullahpsg@gmail.com>
2024-06-30 11:16:16 +0700

    [NEW] header and footer fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -82,8 +82,7 @@
   </div>
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+      <p class="text-gray-500 text-sm text-center sm:text-left">© 2024 Tiketin
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a class="text-gray-500">
