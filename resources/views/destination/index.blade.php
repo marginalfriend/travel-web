7077fb1d7982305ec22bc4c7838fb2a61f392096
marginalfriend <abdullahpsg@gmail.com>
2024-07-01 18:04:51 +0700

    [NEW] image preview

--- resources/views/destination/index.blade.php
+++ resources/views/destination/index.blade.php
@@ -16,7 +16,7 @@
 		@foreach ($destinations as $destination)
 			<div class="">
 				<div class="bg-gray-100 p-6 rounded-lg">
-					<img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+					<img class="h-40 rounded w-full object-cover object-center mb-6" src="{{ $destination -> getImageUrlAttribute()}}" alt="content">
 					<h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{ $destination -> category -> name }}</h3>
 					<h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{ $destination -> name }}</h2>
 					<p class="leading-relaxed text-base">{{ $destination -> description}}</p>
