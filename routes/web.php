7e8fae438e5ec05c235fb96ee21a2bdbda23b394
marginalfriend <abdullahpsg@gmail.com>
2024-07-02 14:32:12 +0700

    [NEW] laravel breeze config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DestinationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +14,18 @@ use App\Http\Controllers\DestinationController;
 |
 */
 
-Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/destination', [DestinationController::class, 'index'])->name('destination.index');
-Route::get('/destination/{destination}', [DestinationController::class, 'view'])->name('destination.view');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/contact', function () { return view('contact'); });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
