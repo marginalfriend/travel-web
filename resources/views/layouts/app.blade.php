e5caa579b1b7f9e3a04b43d623ebcf0047102c60
marginalfriend <abdullahpsg@gmail.com>
2024-07-03 20:20:35 +0700

    [CHANGES] changed web title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Bayah Beach</title>
 		<link rel="shortcut icon" type="image/svg+xml" sizes="114x114" href="{{ asset('favicon.svg') }}">
 
     <!-- Fonts -->
